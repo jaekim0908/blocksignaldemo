9996c7dd6cdf1dc078d767ed13a1640a5e0ba7df
Jae Kim <jaekim0908@gmail.com>
2022-03-14 03:27:49 -0700

    change home page picture

--- pages/home.js
+++ pages/home.js
@@ -110,8 +110,8 @@ const Home = (providers) => {
           </div>
         </div>
 
-        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
-          <Image src="https://rb.gy/vkzpzt" layout="fill" priority />
+        <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-20 right-5">
+          <Image src="https://bit.ly/3t7ns34" layout="fill" priority />
         </div>
       </main>
     </div>
